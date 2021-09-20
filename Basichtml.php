d351a390e457b98f22a18d129b8c214ab14f8a90
MohdZaidBoss <90519286+MohdZaidBoss@users.noreply.github.com>
2021-09-20 17:08:52 +0530

    added registration form

--- Basichtml.php
+++ Basichtml.php
@@ -10,105 +10,60 @@
     </head>
     <body>
         
-   <div class="container">
+   <
+<div class = "container" >
+<h1 class = "text-center text-success">Registration for Attendaces</h1>
 
-   
-<button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-link">Link</button>
-    <a class="btn btn-primary" href="#" role="button">Link</a>
-    <input class="btn btn-primary" type="submit" value="Submit">
-<input class="btn btn-primary" type="reset" value="Reset">  
 
-<div class="container-sm">100% wide until small breakpoint</div>
-<div class="container-md">100% wide until medium breakpoint</div>
-<div class="container-lg">100% wide until large breakpoint</div>
-<label for="customRange1" class="form-label">Example range</label>
-<input type="range" class="form-range" id="customRange1">
+<form method = "get" actiom = "success.php">
+<div class="form-group">
+    <label for="firstname" class="form-label">Firstname</label>
+    <input type="text" class="form-control" id="firstname" >
+ </div>   
+ <br>
+<div class="form-group">
+    <label for="lastname" class="form-label">Lastname</label>
+    <input type="text" class="form-control" id="lastname">
+</div>
 <br>
-<p>
-<div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email address</label>
+
+<div class="form-group">
+    <input type="dob" class="form-check-input" id="dob">
+    <label class="text" for="form-control"> Date Of Birth</label>
 </div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-  <div class="mb-3">
-  <label for="formFile" class="form-label">Default file input example</label>
-  <input class="form-control" type="file" id="formFile">
-</div>
-<div class="mb-3">
-  <label for="formFileMultiple" class="form-label">Multiple files input example</label>
-  <input class="form-control" type="file" id="formFileMultiple" multiple>
-</div>
-<div class="mb-3">
-  <label for="formFileDisabled" class="form-label">Disabled file input example</label>
-  <input class="form-control" type="file" id="formFileDisabled" disabled>
-</div>
-<div class="mb-3">
-  <label for="formFileSm" class="form-label">Small file input example</label>
-  <input class="form-control form-control-sm" id="formFileSm" type="file">
-</div>
-<div>
-  <label for="formFileLg" class="form-label">Large file input example</label>
-  <input class="form-control form-control-lg" id="formFileLg" type="file">
-</div>
-<select class="form-select" aria-label="Default select example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
+<br>
+
+<div class="form-group">
+    <input type="speciality" class="form-check-input" id="speciality">
+<select class="form-control" aria-label="speciality">
+  <option selected>Open this  menu</option>
+  <option value="1">Database Admin</option>
+  <option value="2">App Developer</option>
+  <option value="3">Web Developer</option>
+  <option value="4">Full Stack Developer</option>
+  <option value="5">other</option>
 </select>
-<br>
-<select class="form-select" size="3" aria-label="size 3 select example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-<br>
-
-<div class="input-group">
-  <span class="input-group-text">First and last name</span>
-  <input type="text" aria-label="First name" class="form-control">
-  <input type="text" aria-label="Last name" class="form-control">
 </div>
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<br>
+<div class="form-group">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
+</div>
+  <br>
+<div class="form-group">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password">
+</div>
+<br>
+<div class="form-group">
+    <label for="phone" class="form-label">Phone Number</label>
+    <input type="number" class="form-control" id="phone" >
+    <div id="phone" class="form-text">We'll never share your email with anyone else.</div>
+</div>
+
+</div>
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<br>
-<label for="inputPassword5" class="form-label">Password</label>
-<input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-<div id="passwordHelpBlock" class="form-text">
-  Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-</div>
-<br>
-<div class="row g-3 align-items-center">
-  <div class="col-auto">
-    <label for="inputPassword6" class="col-form-label">Password</label>
-  </div>
-  <div class="col-auto">
-    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-  </div>
-  <div class="col-auto">
-    <span id="passwordHelpInline" class="form-text">
-      Must be 8-20 characters long.
-    </span>
-  </div>
-</div>
-</div>
 </body>
